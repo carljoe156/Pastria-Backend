6d463437e0749b6ba1966b96e364abb1e6d4e2e9
Carl Joseph <carljoseph156@gmail.com>
2025-03-05 10:53:18 -0500

    Added/Updated Product model

---
+++ models/productModel.mjs
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
